4049b0c52dc23730b0f7b28ceee4a53a7b872243
Werapat <werapat@gmail.com>
2020-04-22 13:40:22 +0700

    update

--- data/run.sh
+++ data/run.sh
@@ -49,8 +49,12 @@ if bashio::config.true 'cloudflare'; then
 fi
 
 # Prepare config file
+sed -i "s/%%FULLCHAIN%%/$CERTFILE/g" /etc/nginx.conf
+sed -i "s/%%PRIVKEY%%/$KEYFILE/g" /etc/nginx.conf
 sed -i "s/%%DOMAIN%%/$DOMAIN/g" /etc/nginx.conf
 
+[ -n "$HSTS" ] && HSTS="add_header Strict-Transport-Security \"$HSTS\" always;"
+sed -i "s/%%HSTS%%/$HSTS/g" /etc/nginx.conf
 
 # Allow customize configs from share
 if bashio::config.true 'customize.active'; then
