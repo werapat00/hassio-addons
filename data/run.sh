008439b05381ab34bbe5bba2c3da09e2ede21164
Werapat <werapat@gmail.com>
2020-04-22 14:16:05 +0700

    update new

--- data/run.sh
+++ data/run.sh
@@ -1,28 +1,10 @@
 #!/usr/bin/env bashio
 set -e
 
-DHPARAMS_PATH=/data/dhparams.pem
-
-SNAKEOIL_CERT=/data/ssl-cert-snakeoil.pem
-SNAKEOIL_KEY=/data/ssl-cert-snakeoil.key
-
-CLOUDFLARE_CONF=/data/cloudflare.conf
-
 DOMAIN=$(bashio::config 'domain')
 KEYFILE=$(bashio::config 'keyfile')
 CERTFILE=$(bashio::config 'certfile')
 HSTS=$(bashio::config 'hsts')
-
-# Generate dhparams
-if ! bashio::fs.file_exists "${DHPARAMS_PATH}"; then
-    bashio::log.info  "Generating dhparams (this will take some time)..."
-    openssl dhparam -dsaparam -out "$DHPARAMS_PATH" 4096 > /dev/null
-fi
-
-if ! bashio::fs.file_exists "${SNAKEOIL_CERT}"; then
-    bashio::log.info "Creating 'snakeoil' self-signed certificate..."
-    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout $SNAKEOIL_KEY -out $SNAKEOIL_CERT -subj '/CN=localhost'
-fi
 
 # Prepare config file
 sed -i "s/%%FULLCHAIN%%/$CERTFILE/g" /etc/nginx.conf
