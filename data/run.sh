105238b2b733a29476c7cc9b4420f1f26d8b4089
Werapat <werapat@gmail.com>
2020-04-22 14:08:02 +0700

    -

--- data/run.sh
+++ data/run.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bashio
 set -e
+CLOUDFLARE_CONF=/data/cloudflare.conf
+
 DOMAIN=$(bashio::config 'domain')
 KEYFILE=$(bashio::config 'keyfile')
 CERTFILE=$(bashio::config 'certfile')
 HSTS=$(bashio::config 'hsts')
+
+if bashio::config.true 'cloudflare'; then
+    sed -i "s|#include /data/cloudflare.conf;|include /data/cloudflare.conf;|" /etc/nginx.conf
+    # Generate cloudflare.conf
+    if ! bashio::fs.file_exists "${CLOUDFLARE_CONF}"; then
+        bashio::log.info "Creating 'cloudflare.conf' for real visitor IP address..."
+        echo "# Cloudflare IP addresses" > $CLOUDFLARE_CONF;
+        echo "" >> $CLOUDFLARE_CONF;
+
+        echo "# - IPv4" >> $CLOUDFLARE_CONF;
+        for i in $(curl https://www.cloudflare.com/ips-v4); do
+            echo "set_real_ip_from ${i};" >> $CLOUDFLARE_CONF;
+        done
+
+        echo "" >> $CLOUDFLARE_CONF;
+        echo "# - IPv6" >> $CLOUDFLARE_CONF;
+        for i in $(curl https://www.cloudflare.com/ips-v6); do
+            echo "set_real_ip_from ${i};" >> $CLOUDFLARE_CONF;
+        done
+
+        echo "" >> $CLOUDFLARE_CONF;
+        echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_CONF;
+    fi
+fi
 
 # Prepare config file
 sed -i "s/%%FULLCHAIN%%/$CERTFILE/g" /etc/nginx.conf
