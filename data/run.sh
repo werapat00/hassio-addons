29867dc25173fa4f8638e25bffd8d0779829a23b
Werapat <werapat@gmail.com>
2020-04-22 13:32:59 +0700

    update

--- data/run.sh
+++ data/run.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bashio
 set -e
+
+DHPARAMS_PATH=/data/dhparams.pem
+
+SNAKEOIL_CERT=/data/ssl-cert-snakeoil.pem
+SNAKEOIL_KEY=/data/ssl-cert-snakeoil.key
+
 CLOUDFLARE_CONF=/data/cloudflare.conf
 
 DOMAIN=$(bashio::config 'domain')
 KEYFILE=$(bashio::config 'keyfile')
 CERTFILE=$(bashio::config 'certfile')
 HSTS=$(bashio::config 'hsts')
+
+# Generate dhparams
+if ! bashio::fs.file_exists "${DHPARAMS_PATH}"; then
+    bashio::log.info  "Generating dhparams (this will take some time)..."
+    openssl dhparam -dsaparam -out "$DHPARAMS_PATH" 4096 > /dev/null
+fi
+
+if ! bashio::fs.file_exists "${SNAKEOIL_CERT}"; then
+    bashio::log.info "Creating 'snakeoil' self-signed certificate..."
+    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout $SNAKEOIL_KEY -out $SNAKEOIL_CERT -subj '/CN=localhost'
+fi
 
 if bashio::config.true 'cloudflare'; then
     sed -i "s|#include /data/cloudflare.conf;|include /data/cloudflare.conf;|" /etc/nginx.conf
@@ -33,6 +50,7 @@ fi
 
 # Prepare config file
 sed -i "s/%%DOMAIN%%/$DOMAIN/g" /etc/nginx.conf
+
 
 # Allow customize configs from share
 if bashio::config.true 'customize.active'; then
