723c7caa5088a886134a005de8e46e0f2cdfb26d
Werapat <werapat@gmail.com>
2020-04-22 13:53:54 +0700

    update

--- data/run.sh
+++ data/run.sh
@@ -1,28 +1,11 @@
 #!/usr/bin/env bashio
 set -e
-
-DHPARAMS_PATH=/data/dhparams.pem
-
-SNAKEOIL_CERT=/data/ssl-cert-snakeoil.pem
-SNAKEOIL_KEY=/data/ssl-cert-snakeoil.key
-
 CLOUDFLARE_CONF=/data/cloudflare.conf
 
 DOMAIN=$(bashio::config 'domain')
 KEYFILE=$(bashio::config 'keyfile')
 CERTFILE=$(bashio::config 'certfile')
 HSTS=$(bashio::config 'hsts')
-
-# Generate dhparams
-if ! bashio::fs.file_exists "${DHPARAMS_PATH}"; then
-    bashio::log.info  "Generating dhparams (this will take some time)..."
-    openssl dhparam -dsaparam -out "$DHPARAMS_PATH" 4096 > /dev/null
-fi
-
-if ! bashio::fs.file_exists "${SNAKEOIL_CERT}"; then
-    bashio::log.info "Creating 'snakeoil' self-signed certificate..."
-    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout $SNAKEOIL_KEY -out $SNAKEOIL_CERT -subj '/CN=localhost'
-fi
 
 if bashio::config.true 'cloudflare'; then
     sed -i "s|#include /data/cloudflare.conf;|include /data/cloudflare.conf;|" /etc/nginx.conf
@@ -55,14 +38,6 @@ sed -i "s/%%DOMAIN%%/$DOMAIN/g" /etc/nginx.conf
 
 [ -n "$HSTS" ] && HSTS="add_header Strict-Transport-Security \"$HSTS\" always;"
 sed -i "s/%%HSTS%%/$HSTS/g" /etc/nginx.conf
-
-# Allow customize configs from share
-if bashio::config.true 'customize.active'; then
-    CUSTOMIZE_DEFAULT=$(bashio::config 'customize.default')
-    sed -i "s|#include /share/nginx_proxy_default.*|include /share/$CUSTOMIZE_DEFAULT;|" /etc/nginx.conf
-    CUSTOMIZE_SERVERS=$(bashio::config 'customize.servers')
-    sed -i "s|#include /share/nginx_proxy/.*|include /share/$CUSTOMIZE_SERVERS;|" /etc/nginx.conf
-fi
 
 # start server
 bashio::log.info "Running nginx..."
